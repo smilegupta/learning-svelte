87d00663e3ad9ceeee295fbb58f84448990d661f
smilegupta <guptamiley3012@gmail.com>
2021-09-08 17:33:03 +0530

    Initial Push

---
+++ src/App.svelte
@@ -0,0 +1,36 @@
+<script>
+  import Navbar from "./Navbar.svelte";
+  import Player from "./Player.svelte";
+  import AddPlayer from "./AddPlayer.svelte";
+  let players = [
+    {
+      name: "John Doe",
+      points: 53,
+    },
+    {
+      name: "Sam Smith",
+      points: 45,
+    },
+    {
+      name: "Sara Wilson",
+      points: 34,
+    },
+  ];
+
+  const addPlayer = (e) => {
+    const newPlayer = e.detail;
+    players = [...players, newPlayer];
+  };
+</script>
+
+<Navbar />
+<div class="container">
+  <AddPlayer on:addplayer={addPlayer} />
+  {#if players.length === 0}
+    <p>No Players</p>
+  {:else}
+    {#each players as player}
+      <Player name={player.name} points={player.points} />
+    {/each}
+  {/if}
+</div>
---
+++ src/Player.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let name, points;
+  const addPoint = () => (points += 1);
+  const removePoint = () => (points -= 1);
+  const toggleControls = () => (showControls = !showControls);
+  let showControls = false;
+</script>
+
+<div class="card">
+  <h1>
+    {name}
+    <button class="btn btn-sm" on:click={toggleControls}>
+      {#if showControls} - {:else} + {/if}</button
+    >
+  </h1>
+  <h3>Points: {points}</h3>
+  {#if showControls}
+    <button class="btn" on:click={addPoint}> +1 </button>
+    <button class="btn btn-dark" on:click={removePoint}> -1 </button>
+    <input type="number" bind:value={points} />
+  {/if}
+</div>
+
+<style>
+  h1 {
+    color: #204f6e;
+  }
+
+  h3 {
+    margin-bottom: 10px;
+  }
+</style>
